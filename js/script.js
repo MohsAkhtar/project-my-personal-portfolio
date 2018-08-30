0e07f3d686a064aa0682f804a6d9a9b352ea46d1
40183396 <40183396@live.napier.ac.uk>
2018-08-30 14:46:40 +0100

    Added typed library and applied it to subheading

--- js/script.js
+++ js/script.js
@@ -4,4 +4,16 @@ $(document).ready(function() {
     play: 5000,
     pagination: false
   });
+
+  var typed = new Typed('.typed', {
+    strings: [
+      'Software Developer',
+      'Web Developer',
+      'Computer Science Graduate'
+    ],
+    typeSpeed: 70,
+    loop: true,
+    startDelay: 1000,
+    showCursor: false
+  });
 });
